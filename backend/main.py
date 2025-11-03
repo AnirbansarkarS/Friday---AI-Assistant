f80c23cded4dace59861227d36cd4fa627dea2d5
Anirban Sarkar <sanirban2006@gmail.com>
2025-11-03 19:35:57 +0530

    feat: voice assistant working

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,38 @@
-from fastapi import FastAPI
-from .routes import health, chat, rag
-from .config import HOST, PORT
-import os
 
+from core.speech_listener import listen_command
+from core.nlp_engine import interpret_command
+from core.task_executor import execute_task
+from core.tts_engine import speak
 
-app = FastAPI()
+def main():
+    speak("Friday online. Listening for your command...")
 
+    while True:
+        command = listen_command()
+        if not command:
+            continue
+        print(f"🗣️ You said: {command}")
 
-app.include_router(health.router, prefix="/api")
-app.include_router(chat.router, prefix="/api")
-app.include_router(rag.router, prefix="/api")
-
+        intent, response = interpret_command(command)
+        execute_task(intent, command)
+        speak(response)
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("backend.app:app", host=HOST, port=PORT, reload=True)
+    main()
+# from fastapi import FastAPI
+# from .routes import health, chat, rag
+# from .config import HOST, PORT
+# import os
+
+
+# app = FastAPI()
+
+
+# app.include_router(health.router, prefix="/api")
+# app.include_router(chat.router, prefix="/api")
+# app.include_router(rag.router, prefix="/api")
+
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("backend.app:app", host=HOST, port=PORT, reload=True)
